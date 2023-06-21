6833b70e2413187b713ba027c6a85f32002d942a
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-06-21 14:33:09 -0400

    adding stuff
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- jdbc-boot/src/main/java/io/spring/training/boot/jdbcboot/JdbcBootApplication.java
+++ jdbc-boot/src/main/java/io/spring/training/boot/jdbcboot/JdbcBootApplication.java
@@ -2,12 +2,25 @@ package io.spring.training.boot.jdbcboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class JdbcBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JdbcBootApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner(JdbcTemplate jdbcTemplate) {
+		
+		String Query = "select count(*) from T_ACCOUNT";
+
+		return args -> System.out.println("Hello, there are " + jdbcTemplate.queryForObject(Query, Long.class) + " accounts");
+		
 	}
 
 }
--- jdbc-boot/src/test/java/io/spring/training/boot/jdbcboot/JdbcBootApplicationTests.java
+++ jdbc-boot/src/test/java/io/spring/training/boot/jdbcboot/JdbcBootApplicationTests.java
@@ -1,11 +1,24 @@
 package io.spring.training.boot.jdbcboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import static org.assertj.core.api.Assertions.*;
 @SpringBootTest
 class JdbcBootApplicationTests {
 
+	public static final String Query = "select count(*) from T_ACCOUNT";
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	public void testNumberOfAccounts() {
+		long count = jdbcTemplate.queryForObject(Query, Long.class);
+		assertThat(count).isEqualTo(21);
+	}
 	@Test
 	void contextLoads() {
 	}
